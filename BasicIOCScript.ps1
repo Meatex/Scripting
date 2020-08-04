932f061b13e49ca0126f7d7dbd79034fad755c9d
Meatex <59155747+Meatex@users.noreply.github.com>
2020-08-04 03:40:57 +0000

    Update BasicIOCScript.ps1

--- BasicIOCScript.ps1
+++ BasicIOCScript.ps1
@@ -1,4 +1,4 @@
-[pscredential]$creds = get-credential -message hey -username 'domain\admin'
+[pscredential]$creds = get-credential -message hey -username 'Administrator'
 $comps =@('10.110.3.12','10.110.3.13','10.110.3.14')
 $RunKeys=@("HKLM:\Software\Microsoft\Windows\CurrentVersion\Run\", 
 "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce\",
@@ -12,19 +12,18 @@ $RunKeys=@("HKLM:\Software\Microsoft\Windows\CurrentVersion\Run\",
 "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\run\",
 "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\run\")
 
-$DNS = Invoke-Command -ComputerName $comps -Credential $creds -Authentication Negotiate -ScriptBlock {Get-DnsClientCache}
-$IPs = Invoke-Command -ComputerName $comps -Credential $creds -Authentication Negotiate -ScriptBlock {Get-NetTCPConnection}
+$DNS = Invoke-Command -ComputerName $comps -Credential $creds -Authentication Negotiate -ScriptBlock {Get-DnsClientCache} | select-object pscomputername,data,name,entry
+$IPs = Invoke-Command -ComputerName $comps -Credential $creds -Authentication Negotiate -ScriptBlock {Get-NetTCPConnection} | select-object pscomputername,LocalAddress,LocalPort,RemoteAddress,RemotePort,owningprocess,state
 
 $reg = Invoke-Command -ComputerName $comps -Credential $creds -Authentication Negotiate -ScriptBlock {
     foreach ($key in $using:Runkeys) {
-        $matches = (Get-Item -path $key).GetValueNames()
-        if ($matches -ne $null){Get-ItemProperty -path $key -Name $matches | select-object pscomputername,pspath,$matches}
+        Get-Item -path $key -ErrorAction SilentlyContinue -Force
     }
-}| Select-Object * -ExcludeProperty Runspaceid
+}
 
 $skeddy = Invoke-Command -ComputerName $comps -Credential $creds -Authentication Negotiate -ScriptBlock {
     Get-ScheduledTask | Select-Object pscomputername,Taskname,{$_.Actions.Execute}
-}
+}| select-Object * -ExcludeProperty Runspaceid
 
 $procs = Invoke-Command -ComputerName $comps -Credential $creds -Authentication Negotiate -ScriptBlock {
     get-wmiobject win32_process | select-Object Name,ProcessId,ParentProcessId,CommandLine
@@ -34,12 +33,12 @@ $Users = Invoke-Command -ComputerName $comps -Credential $creds -Authentication 
     Get-LocalUser | select-Object name,enabled,pscomputername,@{label="GroupMembership";expression={net.exe user $_.name | Select-String "Local Group Memberships" }},@{label="LastLogon";expression={net.exe user $_.name | Select-String "Last Logon"}}
 } | select-Object * -ExcludeProperty Runspaceid
 
-echo $DNS
-echo $IPs
-echo $reg
-echo $skeddy
-echo $procs
-echo $users
+$DNS > ./report.txt
+$IPs >> ./report.txt
+$reg >> ./report.txt
+$skeddy >> ./report.txt
+$procs >> ./report.txt
+$users >> ./report.txt
 <#
 $dir = (cmd /c robocopy C:\ null *.* /l /s /njh /njs /ns /fp /lev:12).trim() | select-string "New File" | where {-not [string]::IsNullOrWhiteSpace($_)} |foreach{$_ -replace "`t","" -replace 'New File  ',''} 
 #put hashes in here
